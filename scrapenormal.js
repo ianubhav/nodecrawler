7f04fc6c6ff6a048798228c3271cb0867f6e2c14
Anubhav Agarwal <Viper@Anubhav.local>
2017-06-03 17:25:25 +0530

    first commit

---
+++ scrapenormal.js
@@ -0,0 +1,101 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require("fs");
+var START_URL = "medium.com";
+var outputfile = 'output.csv'
+var pagesVisited = {};
+var numPagesVisited = 0;
+var pagesToVisit = [];
+var totalLoop = 5;
+var expression = /(medium)\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~&//=]*)/gi;
+var regex = new RegExp(expression);
+
+fs.writeFile(outputfile, 'url\n',  function(err) {
+    if (err) {
+        return console.error(err);
+    }
+});
+
+
+function startUrl(url) {
+    // Add page to our set
+    pagesVisited[url] = true;
+    numPagesVisited++;
+    appendfile(outputfile,url)
+    // Make the request
+    console.log("Visiting page no " + numPagesVisited);
+    request('https://'+url, function(error, response, body) {
+        // Check status code (200 is HTTP OK)
+        // console.log("Status code: " + response.statusCode);
+        if(response.statusCode !== 200) {
+          console.log("Status code: " + response.statusCode);
+          callback();
+          return;
+        }
+        // Parse the document body
+        var $ = cheerio.load(body);
+        collectInternalLinks($);
+        // Start 5 concurrent loops
+        for( var i = 0; i < totalLoop; i++ ) {
+            crawl();
+        }
+    });
+}
+
+
+function crawl() {
+    var nextPage = pagesToVisit.pop();
+    if ((nextPage in pagesVisited) || (nextPage == null)) {
+        // We've already visited this page, so repeat the crawl
+        crawl();
+    }
+    else {
+        // New page we haven't visited
+        visitPage(nextPage, crawl);
+    }
+}
+
+function appendfile(path,data) {
+    fs.appendFile(path, data+'\n', function (err) {
+        if (err) {
+          console.log('failed appending '+ data)
+        }
+    })
+}
+
+function visitPage(url, callback) {
+    // Add page to our set
+    pagesVisited[url] = true;
+    numPagesVisited++;
+    appendfile(outputfile,url)
+    // Make the request
+    console.log("Visiting page no " + numPagesVisited);
+    request('https://'+url, function(error, response, body) {
+        // Check status code (200 is HTTP OK)
+        // console.log("Status code: " + response.statusCode);
+        if(response.statusCode !== 200) {
+            console.log("Status code: " + response.statusCode);
+            callback();
+            return;
+        }
+        // Parse the document body
+        var $ = cheerio.load(body);
+        collectInternalLinks($);
+        callback();
+    });
+}
+
+
+function collectInternalLinks($) {
+    var allLinks = $('a');
+    allLinks.each(function() {
+        link = $(this).attr('href').match(regex)
+        if (link != null){
+          pagesToVisit.push(link[0]);
+      }
+  });
+}
+
+startUrl(START_URL)
+
+
